294ff1a79853d816f93828c1641ae12006f40a1e
NarutoBabaeva <NarutoBabaeva@yandex.ru>
2024-05-05 23:28:06 +0300

    commit

--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -1,15 +1,18 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <unistd.h>
+
 typedef struct arguments {
-  int v;
-  int b;
-  int e;
-  int n;
-  int s;
-  int t;
+    int v;
+    int b;
+    int e;
+    int n;
+    int s;
+    int t;
 } arguments;
 
 void output(int argc, char **argv, arguments *arg);
+
 void print_file(FILE *f, arguments *option);
+
 arguments arguments_parser(int argc, char *argv[]);
--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -3,11 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 void output_line(char *line, int n);
 
 typedef struct arguments {
-  int e, i, v, c, l, n, h, s, f, o;
-  char pattern[1024];
-  int len_pattern;
-  int file_count;
+    int e, i, v, c, l, n, h, s, f, o;
+    char pattern[1024];
+    int len_pattern;
+    int file_count;
 } arguments;
