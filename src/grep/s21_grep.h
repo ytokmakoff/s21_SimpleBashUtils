dc57505a491b2565f59fbb11cae0e5d292c6a2e3
ytokmakoff <ytokmakoff@gmail.com>
2024-05-12 18:41:59 +0300

    fix: struct moved to top

--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -4,17 +4,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-void pattern_add(arguments *arg, char *pattern);
-void add_reg_from_file(arguments *arg, char *filepath)
-void print_match(regex_t *reg, char *line, int v);
-arguments arguments_parser(int argc, char *argv[]);
-void output_line(char *line, int n);
-void process_file(arguments arg, char *path, regex_t *reg);
-void output(arguments arg, int argc, char **argv);
-
 typedef struct arguments {
     int e, i, v, c, l, n, h, s, f, o;
     char pattern[1024];
     int len_pattern;
     int file_count;
 } arguments;
+
+void pattern_add(arguments *arg, char *pattern);
+void add_reg_from_file(arguments *arg, char *filepath);
+void print_match(regex_t *reg, char *line, int v);
+arguments arguments_parser(int argc, char *argv[]);
+void output_line(char *line, int n);
+void process_file(arguments arg, char *path, regex_t *reg);
+void output(arguments arg, int argc, char **argv);
