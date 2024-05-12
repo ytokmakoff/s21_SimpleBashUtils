f03dc5fea2b5cc1f7949a5953c009c1a4119bb32
ytokmakoff <ytokmakoff@gmail.com>
2024-05-12 17:26:43 +0300

    feat: add functions in h file

--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -5,6 +5,12 @@
 #include <string.h>
 
 void output_line(char *line, int n);
+void process_file(arguments arg, char *path, regex_t *reg);
+void output_line(char *line, int n);
+arguments arguments_parser(int argc, char *argv[]);
+void print_match(regex_t *reg, char *line, int v);
+void add_reg_from_file(arguments *arg, char *filepath);
+void pattern_add(arguments *arg, char *pattern);
 
 typedef struct arguments {
     int e, i, v, c, l, n, h, s, f, o;
